1fd89bb35ae90b2859d2651e943dab357d2bb09f
Elena Kanash <hird250@gmail.com>
2022-01-23 15:42:40 +0300

    feat: add js to burger

--- portfolio/index.js
+++ portfolio/index.js
@@ -1,2 +1,33 @@
 const burger = document.querySelector('.burger');
-console.log(burger)
+const menu = document.querySelector('.nav');
+const menuClose = document.querySelector ('.nav-close');
+
+
+
+
+burger.addEventListener('click', () => {
+  menu.classList.add('nav_active');
+});
+
+menuClose.addEventListener('click', () => {
+  menu.classList.remove('nav_active');
+});
+
+menu.addEventListener('click', closeMenu);
+
+function closeMenu(event) {
+  if (event.target.classList.contains('nav__link')) {
+    menu.classList.remove('nav_active');
+  }
+}
+
+
+
+
+
+
+
+
+
+//console.log(1)
+
