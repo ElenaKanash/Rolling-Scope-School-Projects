ba2ecec3eaaa7174db9ebcb5b9366ab0ce8dfa10
Elena Kanash <hird250@gmail.com>
2022-02-07 21:55:49 +0300

    feat: add play navItems on click

--- js30/index.js
+++ js30/index.js
@@ -1,9 +1,10 @@
 const navItems = document.querySelectorAll('.nav__item');
+const navItem = document.querySelector('.nav__item');
 const navList = document.querySelector('.nav__list');
 const image = document.querySelector('.main');
 const button = document.querySelector('button');
 const logoPlay = document.querySelector('.logo__img');
-console.log(logoPlay)
+
 
 //change Images on navItem click
 function changeImage(event) {
@@ -14,7 +15,8 @@ function changeImage(event) {
     let currentItem = event.target.dataset.item;
 
     if (event.target.dataset.item ===  currentItem ) {
-      image.style.backgroundImage = `url('./assets/img/${currentItem}.jpg')`
+      image.style.backgroundImage = `url('./assets/img/${currentItem}.jpg')`;
+
     }
 }
 navList.addEventListener ('click', changeImage);
@@ -50,10 +52,10 @@ function playSong (e){
 }
 button.addEventListener('click', playSong);
 
-
-  function playLogo(e) {
-
+// play audio on logo click
+function playLogo(e) {
   image.style.backgroundImage = 'url("./assets/img/forest.jpg")'
+
   logoPlay.classList.toggle('active');
   const isPlay = logoPlay.classList.contains('active');
   if (isPlay) {
@@ -66,9 +68,34 @@ button.addEventListener('click', playSong);
 }
 logoPlay.addEventListener('click', playLogo);
 
+// play navItems on click
+function playNav (e) {
+  navItems.forEach(el =>
+    el.classList.remove('active'));
+    e.target.classList.toggle('active');
+
+    const isPlay = e.target.classList.contains('active');
+    if (isPlay) {
+    //toggleBtn()
+    button.classList.add('pause')
+    let currentItem = e.target.dataset.item;
+    console.log(currentItem)
+    if (e.target.dataset.item === currentItem ) {
+      audio.src = `./assets/audio/${currentItem}.mp3`;
+      audio.currentTime = 0;
+      audio.play();
+  } else {
+    //toggleBtn()
+    button.classList.remove('pause')
+    pauseAudio();
+    }
+  }
+}
+
+navList.addEventListener('click', playNav);
 
 
 
 
 
-
+//audio.src = `./assets/audio/${currentItem}.mp3`;
