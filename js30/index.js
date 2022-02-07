0dda5d36b1fc580edaef0894f3b87c81b34ee99d
Elena Kanash <hird250@gmail.com>
2022-02-07 19:18:15 +0300

    feat: add change button img function on click

--- js30/index.js
+++ js30/index.js
@@ -2,22 +2,32 @@ const navItems = document.querySelectorAll('.nav__item');
 const navList = document.querySelector('.nav__list');
 const image = document.querySelector('.main');
 
-
+//change Images on navItem click
 function changeImage(event) {
   navItems.forEach(el =>
     el.classList.remove('active'));
     event.target.classList.add('active');
 
     let currentItem = event.target.dataset.item;
-    console.log(currentItem);
 
     if (event.target.dataset.item ===  currentItem ) {
-      console.log (event.target.dataset.item )
       image.style.backgroundImage = `url('./assets/img/${currentItem}.jpg')`
     }
 }
 
 navList.addEventListener ('click', changeImage);
+
+// change button img on click
+const button = document.querySelector('button');
+
+function toggleBtn() {
+  button.classList.toggle('pause');
+}
+
+button.addEventListener('click', toggleBtn);
+
+
+
 
 
 
