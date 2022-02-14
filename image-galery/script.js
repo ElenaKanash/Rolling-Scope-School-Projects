187d25b71caf1ee9b8d1b46806cddd2f1eb9e057
Elena Kanash <hird250@gmail.com>
2022-02-14 22:10:20 +0300

    feat: add noworking code to js

--- image-galery/script.js
+++ image-galery/script.js
@@ -1,5 +1,5 @@
 const img = document.querySelector('.img');
-const url = 'https://api.unsplash.com/search/photos?query=winter&per_page=6&orientation=landscape&client_id=pakF7ID8qJ1KmpCgkfsCXoxnIGTgrxlK_bmDhJSJ4sk';
+let url = 'https://api.unsplash.com/search/photos?query=winter&per_page=6&orientation=landscape&client_id=pakF7ID8qJ1KmpCgkfsCXoxnIGTgrxlK_bmDhJSJ4sk';
 
 
 async function getData() {
@@ -9,15 +9,16 @@ async function getData() {
  // console.log(data);
   showData(data);
 }
+
 getData();
 
 const gallery = document.querySelector('.image__items');
-console.log(gallery)
+//console.log(gallery)
 
 function showData(data) {
   gallery.innerHTML = "";
 
-  let arrayImg = data['results'].map((el) => {
+  data.results.map((el) => {
   let image = document.createElement('img');
   image.classList.add('img');
   image.alt = `image`;
@@ -25,6 +26,28 @@ function showData(data) {
   gallery.append(image);
   });
 }
+
+const form = document.querySelector('.form-search');
+//console.log(form)
+const input = document.querySelector('.input');
+//console.log(input)
+
+form.addEventListener ('submit', getValue );
+
+ function getValue (e) {
+  e.preventDefault();
+
+  let SearchUrl = `https://api.unsplash.com/photos/random?query=${input.value}&tag_mode=all&orientation=landscape&extras=url_m&count=30&client_id=pakF7ID8qJ1KmpCgkfsCXoxnIGTgrxlK_bmDhJSJ4sk`;
+
+    console.log(SearchUrl);
+    console.log(input.value);
+
+    gallery.innerHTML = '';
+
+    getData(SearchUrl)
+
+  }
+
 
 
 
